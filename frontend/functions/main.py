760d725987b1f0e7bad14026d494edf09369d5ca
clejara <clejara@alumnos.uai.cl>
2024-10-06 00:45:07 -0300

    cloud function test

--- frontend/functions/main.py
+++ frontend/functions/main.py
@@ -7,7 +7,6 @@ from firebase_admin import initialize_app
 
 initialize_app()
 
-
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+@https_fn.on_call()
+def on_request_example(data=None, context=None):
+    return "Hello world!"
--- frontend/src/context/SessionContext.js
+++ frontend/src/context/SessionContext.js
@@ -1,8 +1,24 @@
 // src/SessionContext.js
 import React, { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { getFunctions, httpsCallable } from "firebase/functions";
+import app from '../firebase/configuration';
 
 export const SessionContext = createContext();
+const functions = getFunctions(app);
+
+
+const callOnRequestExample = async () => {
+    const onRequestExample = httpsCallable(functions, 'on_request_example');
+    try {
+      
+      console.log("check")
+      const result = await onRequestExample();
+      console.log(result.data); // "Hello world!"
+    } catch (error) {
+      console.error('Error calling function:', error);
+    }
+};
 
 export const SessionProvider = ({ children }) => {
   const [sessionId, setSessionId] = useState('');
@@ -11,7 +27,11 @@ export const SessionProvider = ({ children }) => {
     // Generate a unique session ID
     const newSessionId = uuidv4();
     setSessionId(newSessionId);
+
+    callOnRequestExample();
   }, []);
+
+
 
   return (
     <SessionContext.Provider value={sessionId}>
